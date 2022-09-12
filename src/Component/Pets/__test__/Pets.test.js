b183c313acb2e6dfbcc2313c6b8e80a15647ab44
tanishka <tsdineshjai@gmail.com>
2022-09-12 16:34:43 +0530

    mock implementation is done

--- src/Component/Pets/__test__/Pets.test.js
+++ src/Component/Pets/__test__/Pets.test.js
@@ -1,6 +1,25 @@
 import { screen, render } from "@testing-library/react";
-import userEvent from "@testing-library/user-event";
+// import userEvent from "@testing-library/user-event";
 
 import Pets from "../Pets";
+import { rest } from "msw";
+import { setupServer } from "msw/node";
+import catsMock from "../../../mocks/cats.json";
 
-describe("Pets", () => {});
+const server = setupServer(
+	rest.get("http://localhost:4000/cats", (req, res, ctx) => {
+		return res(ctx.status(200), ctx.json(catsMock));
+	})
+);
+
+beforeAll(() => server.listen()); //server starts listening
+afterEach(() => server.resetHandlers());
+afterAll(() => server.close()); //stops the server from listening
+
+describe("Pets", () => {
+	test("Pets component should render five card", async () => {
+		render(<Pets />);
+		const cardComponentsNumber = await screen.findAllByRole("article");
+		expect(cardComponentsNumber.length).toBe(5);
+	});
+});
